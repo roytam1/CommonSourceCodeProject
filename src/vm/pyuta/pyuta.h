f6ca4da3e16b98ee75c456ee17db5e2b1c606d64
roytam1 <roytam@gmail.com>
2025-05-04 23:31:39 +0800

    import 2016-02-04.7z

--- src/vm/pyuta/pyuta.h
+++ src/vm/pyuta/pyuta.h
@@ -29,6 +29,7 @@
 #define USE_AUTO_KEY		5
 #define USE_AUTO_KEY_RELEASE	6
 #define USE_AUTO_KEY_CAPS
+#define USE_DEBUGGER
 #define USE_STATE
 
 #include "../../common.h"
@@ -75,6 +76,11 @@ public:
 	// drive virtual machine
 	void reset();
 	void run();
+	
+#ifdef USE_DEBUGGER
+	// debugger
+	DEVICE *get_cpu(int index);
+#endif
 	
 	// draw screen
 	void draw_screen();
